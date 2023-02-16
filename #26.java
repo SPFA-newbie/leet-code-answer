1ed2fc454005721f047baf3e2d7284ea0d143dd1
shi_shu_huan <1357245654@qq.com>
2023-02-16 22:29:11 +0800

    2023-2-16

---
+++ #26.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n=0;
+        for(int i=1;i<nums.length;i++)
+            if(nums[n]!=nums[i]){
+                n++;
+                nums[n]=nums[i];
+            }
+        return n+1;
+    }
+}
